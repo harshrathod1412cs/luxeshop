110a1153271d29e5179f95d0483ada122616e628
harshrathod <harsh.rathod@consumersketch.in>
2026-05-04 18:34:01 +0530

    worekder update

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -17,7 +17,7 @@ export function register(config) {
     if (publicUrl.origin !== window.location.origin) return;
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `/service-worker.js`;
 
       if (isLocalhost) {
         // On localhost: check if SW still exists
